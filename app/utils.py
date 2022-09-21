4a814f9bee36a594387c56276d5d081b92e6ed52
NevinJoby <s10243461@connect.np.edu.sg>
2022-09-21 14:42:03 +0800

    2ND

--- app/utils.py
+++ app/utils.py
@@ -18,7 +18,7 @@ def head():
 
     st.caption("""
         <p style='text-align: center'>
-        by <a href='https://github.com/medlum'>medlum</a>
+        by <a href='https://github.com/NevinJoby'>medlum</a>
         </p>""", unsafe_allow_html=True)
 
     st.write("""
